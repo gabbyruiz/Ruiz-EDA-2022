6387a5f2936065c18fe761828160286700973674
Gabriella Ruiz <tc1346uq@mnstate.edu>
2022-04-26 13:16:03 -0500

    status data and graph

---
+++ status data.R
@@ -0,0 +1,81 @@
+library(dplyr)
+library(readr)
+library(tidyverse)
+library(googlesheets4)
+
+np_url <- "https://docs.google.com/spreadsheets/d/1hfK3ptAkMU4AiM5PknHe4IebFl29wEWNdMKpc5nEC4M/edit#gid=0"
+np_data <-
+  read_sheet(np_url) %>% 
+  left_join(read_sheet(np_url, sheet = 2)) %>% 
+  print()
+
+houston_url <- "https://docs.google.com/spreadsheets/d/1-VpDe1FBQKK1nWYI_HOV8qVGD1fSHKUoe65E11iqOEU/edit#gid=1776011343"
+
+houston_data <-
+  read_sheet(houston_url) %>% 
+  left_join(read_sheet(houston_url, sheet = 2)) %>% 
+  print()
+
+
+data <-
+  bind_rows(
+    np_data, 
+    houston_data
+  ) %>% 
+  mutate(
+    plot = factor(plot)
+  )
+
+
+data_presence_site <- 
+  data %>% 
+  distinct(site, status) %>% 
+  mutate(present = TRUE) %>% 
+  pivot_wider(
+    names_from = "site", 
+    values_from = "present", 
+    values_fill = FALSE
+  ) %>% 
+  print()
+
+filter(
+  data_presence_site,
+  !Houston,
+  !NP
+) %>% 
+  arrange(status)
+
+
+ggplot(data = data) +
+  geom_jitter(mapping = aes(x = plot, y = status, color = status), alpha = 1)
+
+data %>% 
+  count(site, plot, status, name = "n_spp") %>% 
+  ggplot() +
+  geom_col(mapping = aes(x = status, y = n_spp, fill = status)) +
+  facet_wrap(~ site)
+
+# same thing using geom_bar shortcut
+# data %>% 
+#   ggplot() +
+#   geom_bar(mapping = aes(x = status, fill = status)) +
+#   facet_wrap(~ site)
+
+data %>% 
+  count(site, plot, status, name = "n_spp") %>% 
+  group_by(site) %>% 
+  mutate(n_site = sum(n_spp), p = n_spp/n_site) %>% 
+  group_by(status, site) %>% 
+  summarise(sum_p = sum(p)) %>% 
+  ggplot() +
+  geom_col(mapping = aes(x = site, y = sum_p, fill = status),
+           position = "stack") 
+ 
+
+data %>% 
+  count(site, status) %>% 
+  pivot_wider(names_from = "status", values_from = "n") %>% 
+  select(-site) %>% 
+  as.matrix() %>% 
+  unname() %>% 
+  chisq.test()
